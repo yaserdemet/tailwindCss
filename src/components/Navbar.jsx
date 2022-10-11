26e685c03c61451b966118ae7aece8672cbe401b
yaser <ddemety372@gmail.com>
2022-10-11 23:30:57 +0300

    v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div className="text-white">
-        
-        
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT</h1>
-        <ul className=''>
-            <li>
-                Home
-            </li>
-            <li>
-                Company
-            </li>
-            <li>
-                Resources
-            </li>
-            <li>
-                About
-            </li>
-            <li>
-                Contact
-            </li>
-        </ul>
-        
-        
-        </div>
-  )
-}
+    <div
+      className=" flex justify-between items-center h-24 max-w-[1240px] mx-auto px-
+    4 text-white"
+    >
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT</h1>
+      <ul className="flex hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
 
-export default Navbar
+      <div className="fixed left-0 top-0 w-[60%] h-full border-right-gray-100 ">
+        <ul className="pt-24 uppercase">
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
