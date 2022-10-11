b7a61a2cfb9e99ac61618da1fbe48627adebf0cc
yaser <ddemety372@gmail.com>
2022-10-11 19:28:31 +0300

    tailwindProject

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="text-white">
+        
+        
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT</h1>
+        <ul className=''>
+            <li>
+                Home
+            </li>
+            <li>
+                Company
+            </li>
+            <li>
+                Resources
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact
+            </li>
+        </ul>
+        
+        
+        </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [   "./src/**/*.{js,jsx,ts,tsx}", ],
+  theme: {
+    extend: {},
+    listStyleType : {
+      square : "square"
+    }
+  },
+  plugins: [],
+
+
+  important: true,
+
+  container: {
+    center: true,
+  },
+  
+}
